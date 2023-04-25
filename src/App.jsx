546247e0451cf8e3cba774deccf79706a7aabf59
Baekshook <ecgo0106@naver.com>
2023-04-25 11:48:04 +0900

    ✨ : 지갑 로그인 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import Header from "./components/Header";
 import { useState } from "react";
 
 function App() {
-  const [accout, setAccount] = useState("");
+  const [account, setAccount] = useState("");
 
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-gray-950 text-white">
-        <Header />
+        <Header account={account} setAccount={setAccount} />
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/:tokenId" element={<Detail />} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,20 @@
 import { GiChaingun } from "react-icons/gi";
 import { Link } from "react-router-dom";
 import { BiWallet } from "react-icons/bi";
+import { FaApple } from "react-icons/fa";
 
-export default function Header() {
+export default function Header({ account, setAccount }) {
+  const onClickAccount = async () => {
+    try {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      setAccount(accounts[0]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <header className="max-w-screen-xl mx-auto p-4 flex justify-between items-center font-bold">
       <Link to="/">
@@ -10,16 +22,31 @@ export default function Header() {
           <GiChaingun size={28} />
           <div className="ml-1 text-xl text-cyan-200 ">LIKE</div>
           <div className="ml-1 text-xl text-yellow-200 ">LION</div>
-          <div className="ml-1 text-xl">BCS_3</div>
+          <div className="ml-1 text-xl">BCS3</div>
         </div>
       </Link>
       <div>
-        <button className="flex items-center p-2 bg-gray-800 rounded-full">
-          <div className="bg-main w-6 h-6 rounded-full flex justify-center items-center">
-            <BiWallet />
+        {account ? (
+          <div className="flex items-center p-2 bg-gray-800 rounded-full">
+            <div className="bg-main w-6 h-6 rounded-full flex justify-center items-center">
+              <FaApple />
+            </div>
+            <div className="ml-1">
+              {account.substring(0, 4)}...
+              {account.substring(account.length - 4)}
+            </div>
           </div>
-          <div className="ml-1">Connect</div>
-        </button>
+        ) : (
+          <button
+            className="flex items-center p-2 bg-gray-800 rounded-full"
+            onClick={onClickAccount}
+          >
+            <div className="bg-main w-6 h-6 rounded-full flex justify-center items-center">
+              <BiWallet />
+            </div>
+            <div className="ml-1">Connect</div>
+          </button>
+        )}
       </div>
     </header>
   );
