dc2b33b58a7a3c3537c6e4540cef44290a99ba84
Baekshook <ecgo0106@naver.com>
2023-04-25 13:43:13 +0900

    ✨ : upbit API 연동하여 코인 시세 불러오기

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,27 @@ import { GiChaingun } from "react-icons/gi";
 import { Link } from "react-router-dom";
 import { BiWallet } from "react-icons/bi";
 import { FaApple } from "react-icons/fa";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Header({ account, setAccount }) {
+  const [coinPrice, setCoinPrice] = useState();
+
+  const getCoinPrice = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.upbit.com/v1/ticker?markets=KRW-BTC,%20KRW-ETH,%20KRW-MATIC"
+      );
+      setCoinPrice([
+        { symbol: "BTC", price: response.data[0].trade_price },
+        { symbol: "ETH", price: response.data[1].trade_price },
+        { symbol: "MATIC", price: response.data[2].trade_price },
+      ]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   const onClickAccount = async () => {
     try {
       const accounts = await window.ethereum.request({
@@ -15,6 +34,14 @@ export default function Header({ account, setAccount }) {
       console.error(error);
     }
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      getCoinPrice();
+    }, 4000);
+    return () => clearInterval(interval);
+  }, []); // useEffect로 실시간 시세 불러오기 (4초마다 갱신)
+
   return (
     <header className="max-w-screen-xl mx-auto p-4 flex justify-between items-center font-bold">
       <Link to="/">
@@ -25,9 +52,20 @@ export default function Header({ account, setAccount }) {
           <div className="ml-1 text-xl">BCS3</div>
         </div>
       </Link>
-      <div>
+      <div className="flex items-center">
+        {coinPrice && (
+          <ul className="flex text-gray-400 text-sm">
+            {coinPrice.map((v, i) => {
+              return (
+                <li key={i} className="ml-2">
+                  {v.symbol}: {v.price.toLocaleString()}K₩
+                </li>
+              );
+            })}
+          </ul>
+        )}
         {account ? (
-          <div className="flex items-center p-2 bg-gray-800 rounded-full">
+          <div className="flex items-center p-2 bg-gray-800 rounded-full ml-4">
             <div className="bg-main w-6 h-6 rounded-full flex justify-center items-center">
               <FaApple />
             </div>
@@ -38,7 +76,7 @@ export default function Header({ account, setAccount }) {
           </div>
         ) : (
           <button
-            className="flex items-center p-2 bg-gray-800 rounded-full"
+            className="flex items-center p-2 bg-gray-800 rounded-full ml-4"
             onClick={onClickAccount}
           >
             <div className="bg-main w-6 h-6 rounded-full flex justify-center items-center">
