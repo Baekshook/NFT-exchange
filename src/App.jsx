0f3d16ecf8bfbba873e37b25ee3201acba840843
Baekshook <ecgo0106@naver.com>
2023-04-25 10:48:22 +0900

    ✨ : router 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Detail from "./pages/Detail";
+import Main from "./pages/Main";
+
 function App() {
-  return <div className="bg-red-100">Hello, React</div>;
+  return (
+    <BrowserRouter>
+      <div className="bg-red-100">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/:tokenId" element={<Detail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
