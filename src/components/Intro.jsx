61060e4a7d05bbb81c4a6100787dc69cf5c4185a
Baekshook <ecgo0106@naver.com>
2023-04-25 14:48:27 +0900

    💄 : setting project description

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,4 +1,5 @@
 import { GiChaingun } from "react-icons/gi";
+import { CONTRACT_ADDRESS } from "../web3.config";
 
 export default function Intro() {
   const ranNum = Math.floor(Math.random() * 1000) + 1;
@@ -22,6 +23,16 @@ export default function Intro() {
           <div className="bg-main w-6 h-6 rounded-full flex justify-center items-center ml-3 text-gray-950">
             <GiChaingun size={16} />
           </div>
+        </div>
+        <div className="mt-2 flex items-center text-gray-800">
+          by
+          <div className="text-black ml-2">{CONTRACT_ADDRESS}</div>
+        </div>
+        <div className="mt-2 text-gray-600">
+          다덴부란(.env란 "environment"의 약어)은 소프트웨어 개발에서 자주
+          사용되는 파일 형식 중 하나입니다. 이 파일은 소프트웨어 개발자들이
+          프로젝트에서 사용되는 환경 변수(environment variable)를 저장하는 데
+          사용됩니다.
         </div>
       </div>
     </div>
