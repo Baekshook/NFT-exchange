a6b5ab4b2f260376fb627ff5bdbb56b2c014747f
Baekshook <ecgo0106@naver.com>
2023-04-25 11:26:39 +0900

    💄 : logo styling, logo 링크 작업

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
-
+import { GiChaingun } from "react-icons/gi";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className="max-w-screen-xl mx-auto p-4 flex justify-between items-center font-bold">
+      <Link to="/">
+        <div className="flex items-center text-main">
+          <GiChaingun size={28} />
+          <div className="ml-1 text-xl text-cyan-200 ">LIKE</div>
+          <div className="ml-1 text-xl text-yellow-200 ">LION</div>
+          <div className="ml-1 text-xl">BCS_3</div>
+        </div>
+      </Link>
+      <div>wallet</div>
+    </header>
+  );
 }
